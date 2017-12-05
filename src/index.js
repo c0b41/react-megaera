0bf6b573678c89bd9b6af8e7bab4168f4ac9750b
ayhankuru <ayhankuru@yandex.com.tr>
2017-12-05 13:09:50 +0300

    init

---
+++ src/index.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+
+export default class ReactMegaera extends Component {
+  static propTypes = {
+    template: PropTypes.node.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      error: null,
+      hasError:false
+    }
+
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    this.props.onChange(nextState);
+  }
+
+  componentWillMount(){
+
+    window.onerror =  (message, filename, lineno, colno, err) => {
+    
+      let error = { 
+        title:err.toString(),
+        stack:message,
+      }
+      this.setState({error, hasError: true});    
+    }
+
+  }
+
+  componentDidCatch(error, info) {
+    this.setState({error, hasError: true});
+  }
+
+
+  render() {
+    const { children, template, ...props } = this.props
+
+    if(this.state.hasError){
+      return template
+    }
+
+    return <div {...props}>{children}</div>
+    
+  }
+ 
+}
